c11f1426e46d2bafbd186a904dd4bda7c4d4a5b6
csnyders <craig.snyders@os.uk>
2015-06-30 11:33:44 +0100

    better css fix double transformFunction call when provider is AJAX type fix errors - they weren't displaying at all

--- dist/os-search-templates.js
+++ dist/os-search-templates.js
@@ -2,17 +2,25 @@ angular.module('os-search').run(['$templateCache', function($templateCache) {
   'use strict';
 
   $templateCache.put('templates/os-search.html',
-    "<input class=\"os-search\" type=\"search\" autocomplete=\"on\" placeholder=\"Start typing to search...\"\n" +
+    "<input class=\"os-search\"\n" +
+    "       type=\"search\"\n" +
+    "       autocomplete=\"on\"\n" +
+    "       placeholder=\"Start typing to search...\"\n" +
     "       ng-model=\"searchInput\"/>\n" +
     "<div class=\"os-search-results\" ng-show=\"searchInput.length > 2\">\n" +
-    "    <div ng-repeat=\"(id, data) in searchResults\">\n" +
-    "        <div class=\"os-search-result-header\">{{searchProviders[id].title}}</div>\n" +
-    "        <div class=\"os-search-result\" ng-show=\"data.inProgress\">\n" +
+    "    <div ng-repeat=\"data in searchResults | orderObjectBy:'received'\">\n" +
+    "        <div class=\"os-search-result-header\">{{searchProviders[data.providerId].title}}</div>\n" +
+    "        <div class=\"os-search-result\" ng-if=\"data.inProgress\">\n" +
     "            <p>In progress...</p>\n" +
     "        </div>\n" +
-    "        <div class=\"os-search-result\" ng-repeat=\"result in data.results\">\n" +
-    "            <p ng-if=\"result.text\">{{result.text}} <span ng-if=\"result.locality\">, {{result.locality}}</span></p>\n" +
-    "            <p ng-if=\"result.error\">Error</p>\n" +
+    "        <div class=\"os-search-result\" ng-if=\"data.error\">\n" +
+    "            <p>Error</p>\n" +
+    "        </div>\n" +
+    "        <div class=\"os-search-result\"\n" +
+    "             ng-if=\"data.results\"\n" +
+    "             ng-repeat=\"result in data.results\"\n" +
+    "             ng-click=\"selectResult(result)\">\n" +
+    "            <p ng-if=\"result.text\">{{result.text}}</p>\n" +
     "        </div>\n" +
     "    </div>\n" +
     "</div>"
--- src/directives/os-search-directive.js
+++ src/directives/os-search-directive.js
@@ -21,14 +21,12 @@ var osSearchDirective = function osSearchDirective(observeOnScope, $http, rx, $t
 
             // turn search provider JSON into an rx.Observable, with a URL including the search term
             var observableWithAJAXConfig = function observableWithAJAXConfig(provider, term) {
-                if (Object.prototype.toString.call(provider) !== '[object Object]') {
-                    throw new Error('Search provider required.');
-                }
-
-                var config = angular.copy(provider);
-                config.params = config.params || {};
-                config.providerId = provider.id;
-                delete config.id;
+                var config = {
+                    params: angular.copy(provider.params),
+                    data: angular.copy(provider.data),
+                    url: provider.url,
+                    method: provider.method
+                };
 
                 // inject search term into params and data
                 for (var k in config.params) {
@@ -74,6 +72,7 @@ var osSearchDirective = function osSearchDirective(observeOnScope, $http, rx, $t
                 // reset the search results for each provider whenever input changes
                 $scope.options.providers.forEach(function (provider) {
                     $scope.searchResults[provider.id] = $scope.searchResults[provider.id] || {};
+                    $scope.searchResults[provider.id].providerId = provider.id; // need to save the id because orderObjectBy changes Object into an Array
                     $scope.searchResults[provider.id].results = [];
                 });
 
@@ -131,15 +130,17 @@ var osSearchDirective = function osSearchDirective(observeOnScope, $http, rx, $t
                             } else {
                                 // don't update the UI if the response is from a different search
                             }
+
                         });
 
                     }, function (error) {
-                        $scope.searchResults[error.config.providerId].inProgress = false;
-                        $scope.searchResults[error.config.providerId].results = [];
-                        $scope.searchResults[error.config.providerId].results.error = error.data.error || error.data; // TODO check this logic with a real server error
-                        $scope.searchResults[error.config.providerId].received = new Date();
-                        $scope.searchResults[error.config.providerId].sent = error.sent; // TODO check this is available
-                        console.log('error - sent at '. error.sent);
+                        $scope.searchResults[providerObservable.providerId].inProgress = false;
+                        $scope.searchResults[providerObservable.providerId].results = [];
+                        $scope.searchResults[providerObservable.providerId].error = error.data.error || error.data; // TODO check this logic with a real server error
+                        //$scope.searchResults[providerObservable.providerId].received = new Date();
+                        $scope.searchResults[providerObservable.providerId].received = Infinity; // needs to be infinity so that we can sort errors to the right
+                        $scope.searchResults[providerObservable.providerId].sent = error.sent; // TODO check this is available
+                        console.log('error - sent at '+ error.sent);
 
                     });
                 });
@@ -161,8 +162,21 @@ var osSearchDirective = function osSearchDirective(observeOnScope, $http, rx, $t
                     }).length > 0;
             };
 
+            $scope.clearSearch = function clearSearch() {
 
+                $scope.options.providers.forEach(function(provider) {
+                    $scope.searchResults[provider.id].inProgress = false;
+                    $scope.searchResults[provider.id].results = [];
+                    $scope.searchResults[provider.id].error = undefined;
+                });
 
+                $scope.searchInput = '';
+            };
+
+            $scope.selectResult = function selectResult(result) {
+                console.log('result selected: ', result);
+                $scope.clearSearch();
+            };
         }
     };
 };
