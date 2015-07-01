e2cf3b434b7837deb710c534bbdbeb09e8e057e0
csnyders <craig.snyders@os.uk>
2015-07-01 10:06:09 +0100

    change all namespaces from os-search to osel-search

--- example-requirejs/app.js
+++ example-requirejs/app.js
@@ -1,29 +1,6 @@
-define('app', ['angular', 'os-search', 'os-search-templates'], function(angular) {
+define('app', ['angular', 'osel-search', 'osel-search-templates'], function(angular) {
 
-    var app = angular.module('tiny-app', ['os-search']);
-
-    app.directive('jsonText', function() {
-        return {
-            restrict: 'A',
-            require: 'ngModel',
-            link: function(scope, elem, attr, ngModel) {
-                function into(input) {
-                    return JSON.parse(input);
-                }
-                function out(data) {
-                    return JSON.stringify(data,null,4);
-                }
-                ngModel.$parsers.push(into);
-                ngModel.$formatters.push(out);
-
-                scope.$watch('search', function(newVal) {
-                    while(elem.outerHeight() < elem[0].scrollHeight + parseFloat(elem.css("borderTopWidth")) + parseFloat(elem.css("borderBottomWidth"))) {
-                        elem.height(elem.height()+1);
-                    }
-                });
-            }
-        };
-    });
+    var app = angular.module('tiny-app', ['osel-search']);
 
     app.controller('my-ctrl', ['$scope', function($scope) {
 
