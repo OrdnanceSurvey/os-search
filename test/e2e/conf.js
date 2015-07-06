4f2466ac1a2250932f1391933d9ad87d9ede4d88
csnyders <craig.snyders@os.uk>
2015-07-06 16:23:56 +0100

    integrating circleci and saucelabs

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -1,12 +1,29 @@
+var capabilitiesForBrowser = function capabilitiesForBrowser(browserName, browserVersion) {
+    var capabilities = {
+        'browserName': browserName,
+        'build': process.env.TRAVIS_BUILD_NUMBER,
+        'name': process.env.CIRCLE_PROJECT_USERNAME + '-' + process.env.CIRCLE_PROJECT_REPONAME + '-' + process.env.CIRCLE_BRANCH
+    };
+    if (browserVersion) {
+        capabilities.version = browserVersion;
+    }
+    return capabilities;
+};
 exports.config = {
     sauceUser: process.env.SAUCE_USERNAME,
     sauceKey: process.env.SAUCE_ACCESS_KEY,
 
+    rootElement: 'body',
+    allScriptsTimeout: 40000,
+    getPageTimeout: 40000,
+
     //seleniumAddress: 'http://localhost:4444/wd/hub',
 
-    capabilities: {
-        'browserName': 'chrome'
-    },
+    multiCapabilities: [
+        capabilitiesForBrowser('chrome', '41'),
+        capabilitiesForBrowser('firefox'),
+        capabilitiesForBrowser('safari')
+    ],
 
     // Spec patterns are relative to the current working directly when
     // protractor is called.
@@ -15,6 +32,7 @@ exports.config = {
     // Options to be passed to Jasmine-node.
     jasmineNodeOpts: {
         showColors: true,
-        defaultTimeoutInterval: 30000
+        defaultTimeoutInterval: 360000,
+        includeStackTrace: true
     }
 };
